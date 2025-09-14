d689347b188095f1f83f17e6ce347adb18639ce6
objz <me@objz.dev>
2025-09-14 17:04:54 +0200

    changed import to api to fix compile issues

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -17,9 +17,9 @@ dependencies {
     api("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
     api("com.fasterxml.jackson.core:jackson-core:2.17.2")
     implementation("org.spongepowered:configurate-yaml:4.2.0")
-implementation("com.guicedee.services:slf4j:1.2.2.1")
-    implementation("io.undertow:undertow-core:2.3.12.Final")
-    implementation("io.undertow:undertow-websockets-jsr:2.3.12.Final")
+    implementation("com.guicedee.services:slf4j:1.2.2.1")
+    api("io.undertow:undertow-core:2.3.12.Final")
+    api("io.undertow:undertow-websockets-jsr:2.3.12.Final")
     implementation("org.bouncycastle:bcprov-jdk18on:1.78.1")
     implementation("org.bouncycastle:bcpkix-jdk18on:1.78.1")
 
