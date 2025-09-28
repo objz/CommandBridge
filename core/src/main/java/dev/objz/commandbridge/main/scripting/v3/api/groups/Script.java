87dea3b01a01fb87394796cf71e76131cf950d86
objz <me@objz.dev>
2025-09-28 13:00:36 +0200

    added command parsing

--- core/src/main/java/dev/objz/commandbridge/main/scripting/v3/api/groups/Script.java
+++ core/src/main/java/dev/objz/commandbridge/main/scripting/v3/api/groups/Script.java
@@ -1,10 +1,14 @@
 package dev.objz.commandbridge.main.scripting.v3.api.groups;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import dev.objz.commandbridge.main.scripting.v3.api.Meta;
+import dev.objz.commandbridge.main.scripting.v3.api.ProblemSink;
 import dev.objz.commandbridge.main.scripting.v3.api.constants.ScriptConstants;
-
+import dev.objz.commandbridge.main.scripting.v3.api.groups.commands.CommandStep;
+import dev.objz.commandbridge.main.scripting.v3.api.groups.commands.ResolvedStep;
+import dev.objz.commandbridge.main.scripting.v3.api.groups.commands.StepResolver;
 
 public record Script(
 		Meta<Integer> version,
@@ -16,9 +20,8 @@ public record Script(
 		Permissions permissions,
 		Defaults defaults,
 
-		List<Arg> args
-// List<CommandStep> commands
-) {
+		List<Arg> args,
+		List<CommandStep> commands) {
 	public static Script withDefaults(String name) {
 		return new Script(
 				Meta.isRequired(),
@@ -28,7 +31,24 @@ public record Script(
 				Meta.isOptional(ScriptConstants.ALIASES),
 				Permissions.withDefaults(),
 				Defaults.withDefaults(),
+				List.of(),
 				List.of());
 	}
 
+	public void validate(ProblemSink problems) { 
+		for (int i = 0; i < args.size(); i++) {
+			args.get(i).validate(problems);
+		}
+		for (int i = 0; i < commands.size(); i++) {
+			commands.get(i).validate(problems, i);
+		}
+	}
+
+	public List<ResolvedStep> resolveSteps(ProblemSink problems) { 
+		ArrayList<ResolvedStep> out = new ArrayList<>(commands.size());
+		for (int i = 0; i < commands.size(); i++) {
+			out.add(StepResolver.resolve(commands.get(i), this, problems, i));
+		}
+		return List.copyOf(out);
+	}
 }
