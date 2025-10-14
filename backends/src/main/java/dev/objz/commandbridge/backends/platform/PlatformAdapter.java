0588f744b9ea2e2a79aaf49880d5017894950f38
objz <me@objz.dev>
2025-10-14 18:47:45 +0200

    fixed commandapi shading issues

--- backends/src/main/java/dev/objz/commandbridge/backends/platform/PlatformAdapter.java
+++ backends/src/main/java/dev/objz/commandbridge/backends/platform/PlatformAdapter.java
@@ -2,11 +2,13 @@ package dev.objz.commandbridge.backends.platform;
 
 import java.nio.file.Path;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 public interface PlatformAdapter {
 	record PlatformEnv(Path dataDir) {
 	}
 
-	default void load(PlatformEnv env) throws Exception {
+	default void load(PlatformEnv env, JavaPlugin plugin) throws Exception {
 	}
 
 	void start(PlatformEnv env) throws Exception;
