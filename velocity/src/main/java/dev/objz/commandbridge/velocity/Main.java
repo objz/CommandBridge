f2750472302d061d8b725a847538c2f3170c5d5b
objz <me@objz.dev>
2025-10-15 12:24:31 +0200

    added commandapi initialization on velocity

--- velocity/src/main/java/dev/objz/commandbridge/velocity/Main.java
+++ velocity/src/main/java/dev/objz/commandbridge/velocity/Main.java
@@ -10,6 +10,12 @@ import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPIHandler;
+import dev.jorel.commandapi.CommandAPIPlatform;
+import dev.jorel.commandapi.CommandAPIVelocityConfig;
+import dev.jorel.commandapi.InternalConfig;
+import dev.jorel.commandapi.LoadContext;
 import dev.objz.commandbridge.config.ConfigManager;
 import dev.objz.commandbridge.config.model.VelocityConfig;
 import dev.objz.commandbridge.logging.Log;
@@ -22,6 +28,8 @@ import dev.objz.commandbridge.velocity.ws.MessageRouter;
 import dev.objz.commandbridge.velocity.ws.SessionHub;
 import dev.objz.commandbridge.velocity.ws.WsServer;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.nio.file.Path;
 
 @Plugin(id = "commandbridge", name = "CommandBridge", version = "3.0.0", url = "https://cb.objz.dev", description = "I did it!", authors = {
@@ -30,12 +38,69 @@ public final class Main {
 	private final ProxyServer server;
 	private final Path dataDir;
 	private ConfigManager configManager;
+	private static boolean commandAPILoaded = false;
 
 	@Inject
 	public Main(ProxyServer server, Logger velocityLogger, @DataDirectory Path dataDir) {
 		this.server = server;
 		this.dataDir = dataDir;
 		Log.install(velocityLogger);
+
+		// this is so ugly but I don't think there is a better way
+		if (!commandAPILoaded) {
+			try {
+				CommandAPIVelocityConfig velocityConfig = new CommandAPIVelocityConfig(server, this)
+						.silentLogs(false)
+						.verboseOutput(false);
+
+				InternalConfig internalConfig = new InternalConfig(velocityConfig);
+
+				Field configField = CommandAPI.class.getDeclaredField("config");
+				configField.setAccessible(true);
+				configField.set(null, internalConfig);
+
+				Class<?> internalVelocityConfigClass = Class
+						.forName("dev.jorel.commandapi.InternalVelocityConfig");
+				Object internalVelocityConfig = internalVelocityConfigClass
+						.getDeclaredConstructor(CommandAPIVelocityConfig.class)
+						.newInstance(velocityConfig);
+
+				Class<?> commandAPIVelocityClass = Class
+						.forName("dev.jorel.commandapi.CommandAPIVelocity");
+				CommandAPIPlatform<?, ?, ?> velocityPlatform = (CommandAPIPlatform<?, ?, ?>) commandAPIVelocityClass
+						.getDeclaredConstructor(internalVelocityConfigClass)
+						.newInstance(internalVelocityConfig);
+
+				LoadContext loadContext = new LoadContext(velocityPlatform);
+
+				Constructor<?> handlerConstructor = CommandAPIHandler.class
+						.getDeclaredConstructor(CommandAPIPlatform.class);
+				handlerConstructor.setAccessible(true);
+				handlerConstructor.newInstance(loadContext.platform());
+
+				loadContext.context().run();
+
+				CommandAPIHandler.getInstance().onLoad();
+
+				Field loadedField = CommandAPI.class.getDeclaredField("loaded");
+				loadedField.setAccessible(true);
+				loadedField.set(null, true);
+
+				StringBuilder currentStack = new StringBuilder();
+				for (StackTraceElement e : Thread.currentThread().getStackTrace()) {
+					currentStack.append(e.toString()).append("\n");
+				}
+				Field loadedStackField = CommandAPI.class.getDeclaredField("loadedStack");
+				loadedStackField.setAccessible(true);
+				loadedStackField.set(null, currentStack.toString());
+
+				commandAPILoaded = true;
+				Log.info("CommandAPI loaded");
+
+			} catch (Exception e) {
+				throw new RuntimeException("CommandAPI initialization failed", e);
+			}
+		}
 	}
 
 	@Subscribe
@@ -50,6 +115,8 @@ public final class Main {
 			return;
 		}
 		Log.setDebug(config.debug());
+
+		CommandAPI.onEnable();
 
 		var secret = new SecretLoader(dataDir).loadOrCreate();
 		var auth = new AuthService(secret);
