9a6464a875cff44eed115a515f2e8845b5c72705
objz <me@objz.dev>
2025-12-28 11:46:30 +0100

    added modrinth fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,12 @@
 rootProject.name = "CommandBridge"
 include("paper", "velocity", "core")
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
 gradle.extra["pversion"] = "2.3.5"
 gradle.extra["pluginType"] = "release"
 gradle.extra["pluginVersions"] = listOf("1.20", "1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6", "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6", "1.21.7", "1.21.8", "1.21.9", "1.21.10")
