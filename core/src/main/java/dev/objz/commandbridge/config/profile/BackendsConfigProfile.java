541b6c48d1f8c20e680131a02bb3f4ca9a3427ab
objz <me@objz.dev>
2026-02-24 18:29:37 +0100

    fix: fixed websocket warnings, when redis mode is enabled and via versa

--- core/src/main/java/dev/objz/commandbridge/config/profile/BackendsConfigProfile.java
+++ core/src/main/java/dev/objz/commandbridge/config/profile/BackendsConfigProfile.java
@@ -31,6 +31,8 @@ public final class BackendsConfigProfile implements ConfigProfile<BackendsConfig
             endpointType = d.endpointType();
             ok = false;
         }
+        boolean websocketMode = endpointType == EndpointType.WEBSOCKET;
+        boolean redisMode = endpointType == EndpointType.REDIS;
 
         BackendsConfig.Endpoints endpointsIn = in.endpoints() != null ? in.endpoints() : d.endpoints();
 
@@ -46,7 +48,9 @@ public final class BackendsConfigProfile implements ConfigProfile<BackendsConfig
         } else {
             wsHost = wsHost.trim();
             if (wsHost.startsWith("ws://") || wsHost.startsWith("wss://")) {
-                Log.warn("'endpoints.websocket.host' must NOT include ws:// or wss://");
+                if (websocketMode) {
+                    Log.warn("'endpoints.websocket.host' must NOT include ws:// or wss://");
+                }
                 wsHost = wsHost.replaceFirst("^wss?://", "");
             }
         }
@@ -70,7 +74,9 @@ public final class BackendsConfigProfile implements ConfigProfile<BackendsConfig
         } else {
             redisHost = redisHost.trim();
             if (redisHost.startsWith("redis://") || redisHost.startsWith("rediss://")) {
-                Log.warn("'endpoints.redis.host' must NOT include redis:// or rediss://");
+                if (redisMode) {
+                    Log.warn("'endpoints.redis.host' must NOT include redis:// or rediss://");
+                }
                 redisHost = redisHost.replaceFirst("^rediss?://", "");
             }
         }
